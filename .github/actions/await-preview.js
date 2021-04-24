83ba5daf9eb5dab8aefacd415236aa0d222e5e30
Mitchell Fragala <ms.fragala@gmail.com>
2021-04-24 01:01:44 -0400

    Update workflow

--- .github/actions/await-preview.js
+++ .github/actions/await-preview.js
@@ -1,1 +1,17 @@
-console.log(process.env);
+const fetch = require('node-fetch');
+const statusesUrl = process.env.STATUSES_URL;
+
+let tm = setTimeout(checkStatus, 0);
+
+async function checkStatus() {
+  const [status] = await fetch(statusesUrl).then((res) => res.json());
+  console.log(status);
+
+  if (status && status.state === 'success') {
+    console.log(status.target_url);
+    clearTimeout(tm);
+    return;
+  }
+
+  tm = setTimeout(checkStatus, 1000 * 15);
+}
