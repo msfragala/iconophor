2c69ca91e012c6a4d9fee43fc96cfc73cc48bdb5
Mitchell Fragala <ms.fragala@gmail.com>
2022-11-12 09:49:44 +0100

    Use solid-js for client components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
-import react from '@astrojs/react';
+import solid from '@astrojs/solid-js';
 import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react()],
+  integrations: [solid()],
   adapter: vercel(),
+  markdown: {
+    shikiConfig: {
+      theme: 'dark-plus',
+      wrap: false,
+    },
+  },
 });
