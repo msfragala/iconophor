b8640213fe957fe206de3cad90fc6c5aa68841ea
Mitchell Fragala <ms.fragala@gmail.com>
2023-10-15 15:59:42 -0400

    Fix unpkg path for Sanity Icons

--- src/routes/icons/sanity-icons/[version]/[icon]/+server.ts
+++ src/routes/icons/sanity-icons/[version]/[icon]/+server.ts
@@ -2,5 +2,5 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { iconHandler } from '@/lib/icon-handler';
 
 export const GET: RequestHandler = iconHandler(['version', 'icon'], ({ version, icon }) => {
-	return `https://raw.githubusercontent.com/sanity-io/icons/${version}/export/${icon}.svg`;
+	return `https://raw.githubusercontent.com/sanity-io/icons/v${version}/export/${icon}.svg`;
 });
